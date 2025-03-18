e3f39b92a0c147586029a77532100c63c29aff17
edstry <edsamig289@gmail.com>
2025-03-18 18:46:03 +0300

    ÐÐÑÐИзменил модели BookÐ, ЗукPerson под hibernate сущности(entity)

--- src/main/java/ru/edstry/project1/models/Book.java
+++ src/main/java/ru/edstry/project1/models/Book.java
@@ -1,51 +1,47 @@
 package ru.edstry.project1.models;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import org.hibernate.validator.constraints.Range;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
+@Table(name = "Book")
 public class Book {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id", referencedColumnName = "id")
+    private Person owner;
+
     @NotEmpty(message = "title should not be empty")
     @Size(max = 50, message = "title should not be greater then 50 characters")
+    @Column(name = "title")
     private String title;
+
     @Size(max = 50, message = "author should not be greater then 50 characters")
     @NotEmpty(message = "author should not be empty")
+    @Column(name = "author")
     private String author;
+
     @Min(value = 1000, message = "Year should be greater then 1000")
+    @Column(name = "year")
     private int year;
 
-    public int getId() {
-        return id;
+    public Book() {
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
+    public Book(String title, String author, int year) {
         this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
         this.author = author;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
         this.year = year;
     }
 }
--- src/main/java/ru/edstry/project1/models/Person.java
+++ src/main/java/ru/edstry/project1/models/Person.java
@@ -1,40 +1,41 @@
 package ru.edstry.project1.models;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Person")
 public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Book> books;
 
     @NotEmpty(message = "Fio should not be empty")
     @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    @Column(name = "fio")
     private String fio;
 
     @Min(value = 1900, message = "Year should be greater then 1900")
+    @Column(name = "year_of_birth")
     private int year_of_birth;
 
-    public int getId() {
-        return id;
-    }
+    public Person() {}
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFio() {
-        return fio;
-    }
-
-    public void setFio(String fio) {
+    public Person(String fio, int year_of_birth) {
         this.fio = fio;
-    }
-
-    public int getYear_of_birth() {
-        return year_of_birth;
-    }
-
-    public void setYear_of_birth(int year_of_birth) {
         this.year_of_birth = year_of_birth;
     }
 }
